08a2fe87f223d27a230cd7635aa4a0e18310ff53
jinwoo165 <jinwoo165@gmail.com>
2026-07-20 13:29:53 +0900

    문서관리 앱 게시

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='document-management-v1',APP=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));});
