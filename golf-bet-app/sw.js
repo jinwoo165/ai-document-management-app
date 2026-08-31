08e812ef3ee9b7b68cf94770e0226c9ae0c39819
jinwoo165 <jinwoo165@gmail.com>
2026-08-31 09:42:57 +0900

    Refresh golf app cache for Firebase live version

--- golf-bet-app/sw.js
+++ golf-bet-app/sw.js
@@ -1,7 +1,10 @@
-const CACHE='golf-bet-v3';
+const CACHE='golf-bet-v4';
 const ASSETS=['./','./index.html','./styles.css','./app-part1.js','./app-part2.js','./app-part3.js','./app-part4.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));
