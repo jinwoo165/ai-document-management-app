c518fd0b5162153c11bd560de99d1f42c88e5e4e
jinwoo165 <jinwoo165@gmail.com>
2026-08-31 09:15:40 +0900

    Add golf app service worker

---
+++ golf-bet-app/sw.js
@@ -0,0 +1,8 @@
+const CACHE='golf-bet-v3';
+const ASSETS=['./','./index.html','./styles.css','./app-part1.js','./app-part2.js','./app-part3.js','./app-part4.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));
+});
