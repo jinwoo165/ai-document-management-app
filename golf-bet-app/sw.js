4ed212367a644fea4e4c092c0b13e298645668b7
jinwoo165 <jinwoo165@gmail.com>
2026-08-31 11:32:26 +0900

    Refresh service worker for v11

--- golf-bet-app/sw.js
+++ golf-bet-app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='golf-bet-v10';
-const ASSETS=['./','./index.html','./styles.css','./readable.css','./app-part1.js','./app-firebase-fix.js','./app-part2.js','./app-room-fix.js','./app-part3.js','./app-part4.js','./app-host-viewer.js','./app-personal-view.js','./app-reset-refresh.js','./app-v10.js','./app-v10-current-hole.js','./manifest.webmanifest'];
+const CACHE='golf-bet-v11';
+const ASSETS=['./','./index.html','./styles.css','./readable.css','./app-part1.js','./app-firebase-fix.js','./app-part2.js','./app-room-fix.js','./app-part3.js','./app-part4.js','./app-host-viewer.js','./app-personal-view.js','./app-reset-refresh.js','./app-v11.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
